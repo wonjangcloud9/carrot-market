3bb88338ce46bdaa567960cc71fa6e3eb04a8fac
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-06-12 00:52:20 +0900

    api router

--- libs/client.ts
+++ libs/client.ts
@@ -1,10 +1,3 @@
 import { PrismaClient } from "@prisma/client";
 
-const client = new PrismaClient();
-
-client.user.create({
-  data: {
-    email: "Hi",
-    name: "Hi",
-  },
-});
+export default new PrismaClient();
